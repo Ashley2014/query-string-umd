85bce808a9ad712b73859e66b59d05daa4987e06
xiaoming <1746534101@qq.com>
2022-03-22 09:36:03 +0800

    #### 0.5.0 upgrade "query-string": "7.1.1",

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ## compile "query-string": "6.12.1",
 ## https://www.npmjs.com/package/query-string
 #### 0.4.0 switch to webpack build
+#### 0.5.0 upgrade "query-string": "7.1.1",
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,30 +1,31 @@
 const path = require('path');
 module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
-    resolve: {
-      mainFields: ['browser', 'module', 'main']
-    },
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
-        library: 'queryString',
-        libraryTarget: 'umd',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                include: /(node_modules|bower_components|dist)/,
-                exclude: /dfefegfe/,
-                use: {
-                    loader: 'babel-loader',
-                    // options: {
-                    //     presets: ['@babel/preset-env']
-                    // }
-                }
-            }
-        ],
-    },
+  mode: 'production',
+  entry: './src/index.js',
+  resolve: {
+    mainFields: ['browser', 'module', 'main']
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    library: 'queryString',
+    libraryTarget: 'umd',
+    // libraryExport: "default",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        include: /(node_modules|bower_components|dist)/,
+        exclude: /dfefegfe/,
+        use: {
+          loader: 'babel-loader',
+          // options: {
+          //     presets: ['@babel/preset-env']
+          // }
+        }
+      }
+    ],
+  },
 };
